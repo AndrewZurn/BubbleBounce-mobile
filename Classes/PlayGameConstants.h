3794776765c39512047de62ff71badcbc4c7dbd0
Andrew Zurn <awzurn@gmail.com>
2014-05-01 16:53:16 +0000

    PlayConstants - Altered GPS ids iOS

--- Classes/PlayGameConstants.h
+++ Classes/PlayGameConstants.h
@@ -10,18 +10,19 @@
 #define GooglePlayGameServices_PlayGameConstants_h
 
 // ClientID and AppID
-#define kClientID "944820157749-29paf58ubekh6p5u88mn8q82fovu60ph.apps.googleusercontent.com" // OAuth 2.0 w/o App Store ID
-#define applicationID "944820157749"
+#define kClientID "255053293081.apps.googleusercontent.com" // OAuth 2.0 w/o App Store ID
+#define applicationID "255053293081"
 
 // Leaderboards
-#define LEADERBOARD "CgkItfqt3b8bEAIQAA"
+#define LEADERBOARD "CgkImczxkrYHEAIQBQ"
 
 // Achievements
-#define ACH_POPPING_DA_BUBBLE "CgkItfqt3b8bEAIQAw"
-#define ACH_NIFTY_FIFTY "CgkItfqt3b8bEAIQBA"
-#define ACH_BUBBLE_BUSTER "CgkItfqt3b8bEAIQBQ"
-#define ACH_YOU_DA_BOMB "CgkItfqt3b8bEAIQBg"
-#define ACH_BUBBLE_STAR "CgkItfqt3b8bEAIQBw"
+#define ACH_POPPING_DA_BUBBLE "CgkImczxkrYHEAIQAA"
+#define ACH_NIFTY_FIFTY "CgkImczxkrYHEAIQAQ"
+#define ACH_BUBBLE_BUSTER "CgkImczxkrYHEAIQAg"
+#define ACH_YOU_DA_BOMB "CgkImczxkrYHEAIQAw"
+#define ACH_BUBBLE_STAR "CgkImczxkrYHEAIQBA"
+
 
 //AdMob
 #define MY_BANNER_UNIT_ID "ca-app-pub-7670697324095342/6854420116"
