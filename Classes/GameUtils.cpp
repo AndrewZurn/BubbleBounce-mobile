2cd6385e4b270a939f756487662d52193491bf10
AndrewZurn <awzurn@gmail.com>
2014-07-31 20:35:02 -0500

    Adding the skeleton for new GameUtil class

--- Classes/GameUtils.cpp
+++ Classes/GameUtils.cpp
@@ -1,1 +1,25 @@
-Will be future class for a utilities class to hold commonly accessed code.
+//
+//  GameUtils.cpp
+//  BouncingBallsCPP
+//
+//  Created by Andrew Zurn on 7/31/14.
+//
+//
+
+#include "GameUtils.h"
+
+std::vector<const char*> getBackgrounds()
+{
+}
+
+const char* getRandomPopSound()
+{
+}
+
+cocos2d::ccColor4F getOppositeColor4F(const char*)
+{
+}
+
+cocos2d::ccColor3B getOppositeColor3B(const char*)
+{
+}
--- Classes/GameUtils.h
+++ Classes/GameUtils.h
@@ -1,1 +1,23 @@
-Will be future header class for a utilities class to hold commonly accessed code.
+//
+//  GameUtils.h
+//  BouncingBallsCPP
+//
+//  Created by Andrew Zurn on 7/31/14.
+//
+//
+
+#ifndef __BouncingBallsCPP__GameUtils__
+#define __BouncingBallsCPP__GameUtils__
+
+#include <iostream>
+#include "cocos2d.h"
+
+class GameUtils {
+public:
+    static std::vector<const char*> getBackgrounds();
+    static const char* getRandomPopSound();
+    static cocos2d::ccColor4F getOppositeColor4F(const char*);
+    static cocos2d::ccColor3B getOppositeColor3B(const char*);
+};
+
+#endif /* defined(__BouncingBallsCPP__GameUtils__) */
